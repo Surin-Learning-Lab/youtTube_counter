930753aa0e4c4660849b4958d65d14b1a66fca51
Surin-Learning-Lab <155703944+Surin-Learning-Lab@users.noreply.github.com>
2025-06-29 18:05:31 +0700

    Update secrets.h

--- youTube_counter/secrets.h
+++ youTube_counter/secrets.h
@@ -1,5 +1,5 @@
 // secrets.h
 const char* ssid = "Surinlearninglab-2.4G";
-const char* password = "SurinLL2023";
-const char* YT_API_KEY = "AIzaSyBto4zDN4MevlBvxtn6v1091sz67nG2_Tc";
-const char* YT_CHANNEL_ID = "UCJviHo3Tk725rB2YYfxzq9g";
+const char* password = "@@@@@@@@@@@@@@@@";
+const char* YT_API_KEY = "##############";
+const char* YT_CHANNEL_ID = "333333333333333333333";
